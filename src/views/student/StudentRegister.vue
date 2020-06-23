7e780e7ad6a06fb1fad70f79fac1df5c9b886963
严俊东 <1172482914@qq.com>
2020-06-23 12:53:36 +0800

    注册页面改版完成

--- src/views/student/StudentRegister.vue
+++ src/views/student/StudentRegister.vue
@@ -1,104 +1,122 @@
 <template>
   <div class="StudentRegister">
-    <div class="bg"></div>
-    <div>
-      <!-- 头部 -->
-      <div class="title-area">
-        <img
-          class="left-img"
-          src="@/assets/img/student/register/leftgaga@2x.png"
-          alt=""
-        />
-        <h2>恭喜您！</h2>
-        <img
-          class="right-img"
-          src="@/assets/img/student/register/leftgaga@2x.png"
-          alt=""
-        />
+    <div class="info" v-if="!loading && !showForm">
+      <template v-if="student.status === 0">
+        <div class="submit-success">
+          <img style="max-width: 100%" :src="submitSuccess" alt="提交成功" />
+        </div>
+      </template>
+      <template v-else>
+        <img src="@/assets/img/success.png" alt="" />
+        <p>资料已审核通过</p>
+      </template>
+      <!-- <img src="../" alt=""> -->
+    </div>
+    <div v-if="showForm">
+      <div class="register-home-page" v-if="!next">
+        <img :src="registerHome" alt="注册" />
+        <div class="btn-area" @click="next = true"></div>
       </div>
-<!-- 中间 -->
-      <div class="middle-area">
-        <div class="box">
-          <img
-                  class="right-img"
-                  src="@/assets/img/student/register/heart@2x.png"
-                  alt=""
-          />
-          <p>成功领取到一节50分钟一对一陪练课！</p>
-          <p>
-            接下来请完成宝贝的基本信息，我们将根据宝贝的自身特色选择合适的优质教师给您！
-          </p>
-        </div>
-      </div>
-      <!-- 底部 -->
-      <div class="bottom-area">
-        <div class="input-item">
-          <span class="label">宝贝的姓名：</span>
-          <input type="text" v-model="formData.name">
-        </div>
-        <div class="input-item">
-          <span class="label">宝贝的年龄：</span>
-          <input type="text" v-model="formData.age">
-        </div>
-        <div class="input-item">
-          <span class="label">宝贝的学琴年限：</span>
-          <input type="text" v-model="formData.studyAge">
-        </div>
-        <div class="input-item">
-          <span class="label">宝贝需要陪练的乐器是：</span>
-          <input type="text" v-model="formData.instrumentStr">
-        </div>
-        <div class="input-item">
-          <span class="label">家长的手机号：</span>
-          <input type="text" v-model="formData.phone">
+      <div class="form-area" v-if="next">
+        <div class="bg"></div>
+        <div>
+          <!-- 头部 -->
+          <div class="title-area">
+            <img
+              class="left-img"
+              src="@/assets/img/student/register/leftgaga@2x.png"
+              alt=""
+            />
+            <h2>恭喜您！</h2>
+            <img
+              class="right-img"
+              src="@/assets/img/student/register/leftgaga@2x.png"
+              alt=""
+            />
+          </div>
+          <!-- 中间 -->
+          <div class="middle-area">
+            <div class="box">
+              <img
+                class="right-img"
+                src="@/assets/img/student/register/heart@2x.png"
+                alt=""
+              />
+              <p>成功领取到一节50分钟一对一陪练课！</p>
+              <p>
+                接下来请完成宝贝的基本信息，我们将根据宝贝的自身特色选择合适的优质教师给您！
+              </p>
+            </div>
+          </div>
+          <!-- 底部 -->
+          <div class="bottom-area">
+            <div class="input-item">
+              <span class="label">宝贝的姓名：</span>
+              <div>
+                <input type="text" v-model="formData.name" />
+              </div>
+            </div>
+            <div class="input-item">
+              <span class="label">宝贝的年龄：</span>
+              <div>
+                <input type="text" v-model="formData.age" />
+              </div>
+            </div>
+            <div class="input-item">
+              <span class="label">宝贝的学琴年限：</span>
+              <div>
+                <input type="text" v-model="formData.studyAge" />
+              </div>
+            </div>
+            <div class="input-item instrumentStr">
+              <span class="label ">宝贝需要陪练的乐器是：</span>
+              <div>
+                <input type="text" v-model="formData.instrumentStr" />
+              </div>
+            </div>
+            <div class="input-item">
+              <span class="label">家长的手机号：</span>
+              <div>
+                <input type="text" v-model="formData.phone" />
+              </div>
+            </div>
+            <div class="register-btn-area">
+              <img
+                class="register-btn"
+                :src="registerBtn"
+                @click="submit"
+                alt="提交"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-<!--    <div class="info" v-if="!loading && !showForm">-->
-<!--      <template v-if="student.status === 0">-->
-<!--        <img src="@/assets/img/warning.png" alt="" />-->
-<!--        <p>正在审核您的资料中...</p>-->
-<!--      </template>-->
-<!--      <template v-else>-->
-<!--        <img src="@/assets/img/success.png" alt="" />-->
-<!--        <p>资料已审核通过</p>-->
-<!--      </template>-->
-<!--      &lt;!&ndash; <img src="../" alt=""> &ndash;&gt;-->
-<!--    </div>-->
-<!--    <div class="StudentForm" v-if="showForm">-->
-<!--      <mt-field-->
-<!--        label="姓名"-->
-<!--        placeholder="请输入您孩子的姓名"-->
-<!--        v-model="formData.name"-->
-<!--      ></mt-field>-->
-<!--      <mt-field-->
-<!--        label="手机号"-->
-<!--        placeholder="请输入手机号"-->
-<!--        type="tel"-->
-<!--        v-model="formData.phone"-->
-<!--      ></mt-field>-->
-<!--      <div class="btn-area mt20">-->
-<!--        <mt-button type="primary" @click.native="submit">提 交</mt-button>-->
-<!--      </div>-->
-<!--    </div>-->
   </div>
 </template>
 
 <script>
 import { studentsList, studentRegister } from "@/api";
 import { Indicator } from "mint-ui";
+import registerBtn from "@/assets/img/student/register/register_btn@2x.png";
+import submitSuccess from "@/assets/img/student/register/submit_success.jpg";
+import registerHome from "@/assets/img/student/register/register_home.jpg";
 
 export default {
   name: "StudentRegister",
   components: {},
   data() {
     return {
+      registerBtn,
+      submitSuccess,
+      registerHome,
+      next: false,
       formData: {
         name: "",
         phone: "",
         instrumentStr: "",
         studyAge: "",
-        age: "",
+        age: ""
       },
       loading: true,
       showForm: false,
@@ -118,7 +136,7 @@ export default {
       });
       Indicator.open();
       /*
-    1. 查询是否已注册 
+    1. 查询是否已注册
       已注册 - 通过审核
       已注册 - 为通审核
       已注册 - 审核被拒
@@ -147,20 +165,41 @@ export default {
     },
     submit() {
       const params = { ...this.formData };
+      Object.keys(params).forEach(key => {
+        if (typeof params[key] === "string") {
+          params[key] = params[key].trim();
+        }
+      });
+      const { studyAge, age } = params;
       if (!params.name) {
-        this.$toast("请输入姓名");
+        return this.$toast("请输入宝贝的姓名");
       } else if (!params.phone) {
-        this.$toast("请输入手机号码");
-      } else {
-        this.$indicator.open();
-        studentRegister(params).then(res => {
-          this.$indicator.close();
-          if (!res) return;
-          this.student = res;
-          this.showForm = false;
-          // this.init();
-        });
+        return this.$toast("请输入手机号码");
+      } else if (!/^1[3456789]\d{9}$/.test(params.phone)) {
+        return this.$toast("请输入正确的手机号码");
       }
+      // 校验 学琴年限（studyAge）
+      // 校验 年龄（age）
+      if (studyAge.length && Number.isNaN(Number(studyAge))) {
+        return this.$toast("学琴年限请输入数字");
+      }
+      if (studyAge.length && (Number(studyAge) < 0 || Number(studyAge > 100))) {
+        return this.$toast("请输入正常的学琴年限！");
+      }
+      if (age.length && Number.isNaN(Number(age))) {
+        return this.$toast("年龄请输入数字");
+      }
+      if (age.length && (Number(age) < 0 || Number(age > 100))) {
+        return this.$toast("请输入正常的年龄！");
+      }
+      this.$indicator.open();
+      studentRegister(params).then(res => {
+        this.$indicator.close();
+        if (!res) return;
+        this.student = res;
+        this.showForm = false;
+        this.init();
+      });
     }
   },
   beforeDestroy() {
@@ -172,85 +211,162 @@ export default {
 @import "../../assets/less/base";
 
 .StudentRegister {
-  .bg {
-    height: 100vh;
-    width: 100vh;
+  .submit-success {
+    font-size: 0;
+  }
+
+  .register-home-page {
+    position: relative;
+    font-size: 0;
+
+    img {
+      max-width: 100%;
+    }
+
+    .btn-area {
+      position: absolute;
+      /*border: 1px solid #000;*/
+      .w(92 * 2);
+      .h(90 * 2);
+      .positionTop(207 * 2);
+      .positionRight(28 * 2);
+    }
+  }
+
+  .info {
     position: fixed;
-    top: 0;
+    width: 100%;
+    height: 100%;
     left: 0;
-    z-index: -1;
-    background: #7dc0da;
-  }
-  /*padding: 15px;*/
-  .pdd(15, 75, 15, 75);
-
-  .title-area {
-    .h(109);
+    top: 0;
     display: flex;
-    .left-img {
-      height: 100%;
-    }
-    .right-img {
-      height: 100%;
-    }
-    h2 {
-      flex: 1;
-      color: #ff0000;
-      .fs(48);
-      text-align: center;
-    }
-  }
-  .middle-area {
-    display: flex;
-    align-items: center;
-    .mt(10);
-    .h(240);
-    .brd(3, solid,rgba(255,255,255,1));
-    .borderRadius(1px);
-    .box {
-      text-align: center;
-      .h(195);
-      background: #fff;
-      .fs(28);
-      img {
-        float: right;
-        .w(61);
-        .mr(29);
-        .mt(29);
-      }
-      p {
-        .pdd(3, 6, 3, 6);
-        line-height: 1.5;
-      }
-    }
-  }
+    justify-content: center;
+    flex-wrap: wrap;
+    align-content: center;
 
-  .bottom-area {
-    .w(600);
-    .h(810);
-    background: #fff;
-  }
-
-  /*.info {*/
-  /*  position: fixed;*/
-  /*  width: 100%;*/
-  /*  height: 100%;*/
-  /*  left: 0;*/
-  /*  top: 0;*/
-  /*  display: flex;*/
-  /*  justify-content: center;*/
-  /*  flex-wrap: wrap;*/
-  /*  align-content: center;*/
-
-  /*  p {*/
-  /*    width: 100%;*/
-  /*    text-align: center;*/
-  /*  }*/
-  /*}*/
-
-  .btn-area {
-    button {
+    p {
       width: 100%;
+      text-align: center;
+    }
+  }
+
+  .form-area {
+    .bg {
+      height: 100vh;
+      width: 100vh;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      background: #7dc0da;
+    }
+
+    /*padding: 15px;*/
+    .pdd(15, 75, 15, 75);
+
+    .title-area {
+      .h(109);
+      display: flex;
+
+      .left-img {
+        height: 100%;
+      }
+
+      .right-img {
+        height: 100%;
+      }
+
+      h2 {
+        flex: 1;
+        color: #ff0000;
+        .fs(48);
+        text-align: center;
+      }
+    }
+
+    .middle-area {
+      display: flex;
+      align-items: center;
+      .mt(10);
+      .h(240);
+      .brd(3, solid, rgba(255, 255, 255, 1));
+      .borderRadius(1px);
+
+      .box {
+        text-align: center;
+        .h(195);
+        background: #fff;
+        .fs(28);
+
+        img {
+          float: right;
+          .w(61);
+          .mr(29);
+          .mt(29);
+        }
+
+        p {
+          .pdd(3, 6, 3, 6);
+          line-height: 1.5;
+        }
+      }
+    }
+
+    .bottom-area {
+      .w(600);
+      .h(710);
+      overflow: hidden;
+      background: #fff;
+      .pl(20);
+      .pr(20);
+      .mt(30);
+
+      .input-item {
+        display: flex;
+        .mt(60);
+        .fs(28);
+
+        span.label {
+          font-weight: bold;
+        }
+
+        div {
+          flex: 1;
+          width: 100%;
+
+          input {
+            display: block;
+            border: none;
+            border-bottom: 1px solid #868686;
+            box-shadow: none;
+            outline: none;
+            width: 100%;
+            .maxW(400);
+          }
+        }
+
+        &.instrumentStr {
+          span.label {
+            .w(154 * 2);
+          }
+        }
+      }
+
+      .register-btn-area {
+        .mt(60);
+        text-align: center;
+
+        .register-btn {
+          .w(200);
+          cursor: pointer;
+        }
+      }
+    }
+
+    .btn-area {
+      button {
+        width: 100%;
+      }
     }
   }
 }
